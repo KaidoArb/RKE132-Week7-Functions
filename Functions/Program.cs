c89ab65ed3156c31e93e6f39ca296b4d30db56ee
Kaido Arb <kaido.arb@tktk.ee>
2023-12-18 23:31:15 +0200

    add week7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Are you coming or leavning? (in/out):");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else if(userChoice == "out")
+{
+    PrintGoodBye();
+}
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, alligator.");
+}
